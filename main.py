c56c4d1e6c7d2ff81ddddd0ba063054f07bd565d
pv2303 <paulovitoorr@outlook.com>
2025-09-12 11:07:16 -0300

    fix: esqueci da barra (:

--- main.py
+++ main.py
@@ -8,14 +8,14 @@ async def root():
 
 # Usage of path parameters
 
-@app.get("items/{item_id}")
+@app.get("/items/{item_id}")
 async def read_item(item_id: int):
     return {"item_id": item_id}
 
-@app.get("users/me")
+@app.get("/users/me")
 async def read_user_me():
     return {"user_id": "the current user"}
 
-@app.get("users/{user_id}")
+@app.get("/users/{user_id}")
 async def read_user_id(user_id: str):
     return {"user_id": user_id}
